64c770e3fd518326546dd30e0292674c15b30646
jluis12369 <jose2021luiz@hotmail.com>
2023-11-28 22:43:55 -0400

    Descreva suas alterações aqui

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -14,9 +14,15 @@ export const Home = () => {
 
     return (
         <LinearGradient colors={['#E1F2E0', '#8DDCE5', '#114A55']} style={styles.container}>
+            <View style= {styles.topContent}>
             <Text style={styles.texto1}>Bem estar em </Text>
             <Text style={styles.numero2}>5</Text>
+            </View>
+            <View style={styles.iniciarContainer}>
+                <Text style={styles.iniciar}>Começar</Text>
+            </View>
         </LinearGradient>
+       
     );
 };
 
@@ -24,18 +30,42 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         flex: 1,
+        
          // para centralizar o conteúdo
     },
+    topContent:{
+        flex: 1,
+        
+        alignItems: 'center',
+    },
     texto1: {
-        marginTop: 90,
+        padding: 30,
+        marginTop: 150,
         fontFamily: 'Roboto',
-        fontSize: 20, // Ajuste conforme necessário
-        color: '#000', // Ajuste a cor conforme necessário
+        fontSize: 40, // Ajuste conforme necessário
+        color: '#114A55', // Ajuste a cor conforme necessário
     },
     numero2: {
         fontFamily: 'Roboto',
-        fontSize: 20, // Ajuste conforme necessário
-        color: '#000', // Ajuste a cor conforme necessário
+        fontSize: 55, // Ajuste conforme necessário
+        color: '#114A55', // Ajuste a cor conforme necessário
+    },
+    iniciarContainer: {
+       
+        justifyContent: 'flex-end',
+        alignItems: 'center', // Para centralizar horizontalmente
+        marginBottom: 120, // Espaço na parte inferior
+        borderWidth: 3,
+        borderColor: '#8DDCE5',
+        backgroundColor: '#8DDCE5',
+        padding: 5,
+        borderRadius:30,
+    },
+    iniciar: {
+        fontFamily: 'Roboto',
+        fontSize: 20,
+        color:'#114A55'
+        
     }
 });
 

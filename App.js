cfdf08c91e12fec1a04d54c4ec834e4696d78247
RyanMatheusNeto <127550801+RyanMatheusNeto@users.noreply.github.com>
2023-11-29 22:30:53 -0300

    modificações ryan (parte da paginação

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/screens/Home/index";
+import {NavigationContainer} from "@react-navigation/native"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Login } from "./src/screens/Login";
+import { View, Text } from "react-native-web";
+
+
+
+const Tab = createBottomTabNavigator();
+
+function Home1 (){
+
+return (
+  <View>
+
+    <Text>
+      hi omomd
+
+
+      5.515
+    </Text>
+  </View>
+
+)
+
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home1} />
+      <Tab.Screen name="Settings" component={Login} />
+    </Tab.Navigator>
+  );
+}
+
 
 export default function App() {
   return (
-    <>
-      <Home/>
-      <StatusBar style="auto"/>
-    </>
+    
+      <NavigationContainer>
+        < MyTabs/>
+      </NavigationContainer>
   );
 }

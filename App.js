e3b77b25318211758b57343e480b55ecb83b5181
RyanMatheusNeto <127550801+RyanMatheusNeto@users.noreply.github.com>
2023-11-29 23:15:24 -0300

    quase acabando as telas

--- App.js
+++ App.js
@@ -3,33 +3,74 @@ import { Home } from "./src/screens/Home/index";
 import {NavigationContainer} from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Login } from "./src/screens/Login";
-import { View, Text } from "react-native-web";
+import { View, Text, TouchableOpacity} from "react-native-web";
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 
-const Tab = createBottomTabNavigator();
 
-function Home1 (){
+
+function Home1 ({navigation}){
 
 return (
   <View>
 
     <Text>
       hi omomd
-
-
-      5.515
     </Text>
-  </View>
+    {[1,2,3,4,5].map((item) => {
+      return (
+        <TouchableOpacity 
+        key={item}
+        onPress={()=> {
+          navigation.navigate('Homecerta')
+        }}
+        >
 
-)
+          <Text>
+            Podcast: 0{item}
+          </Text>
+        </TouchableOpacity>
+
+      )
+    })}
+  </View>
+)}
+
+function Login2 (){
+
+  return (
+    <View>
+  
+      <Text>
+        hi omomd
+  
+  
+        5.515
+      </Text>
+    </View>
+  )
 
 }
+//////////////////////////////////
+
+const HomeStack = createStackNavigator();
+
+function HomeTabStack() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home1" component={Home1} />
+      <HomeStack.Screen name="Homecerta" component={Home} />
+    </HomeStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home1} />
+      <Tab.Screen name="HomeTabStack" component={HomeTabStack} />
       <Tab.Screen name="Settings" component={Login} />
     </Tab.Navigator>
   );
